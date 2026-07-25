647cbecd0f7f54fecbe907c77a85770b029d4237
Chaitanya Shinde <168747560+chaitanyashinde468@users.noreply.github.com>
2026-07-25 20:16:38 +0530

    Added JavaScript Variables Examples

---
+++ 01-Variables/variables.js
@@ -0,0 +1,26 @@
+// var
+
+var name = "Chaitanya";
+
+console.log(name);
+
+// redeclare
+
+var city = "Pune";
+var city = "Mumbai";
+
+console.log(city);
+
+// let
+
+let age = 20;
+
+age = 21;
+
+console.log(age);
+
+// const
+
+const company = "APSS Media";
+
+console.log(company);
