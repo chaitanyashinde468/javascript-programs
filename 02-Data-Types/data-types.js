19977ce842efad9d0156ebe74b8f75983c4cf4aa
Chaitanya Shinde <168747560+chaitanyashinde468@users.noreply.github.com>
2026-07-26 00:31:50 +0530

    Update data-types.js

--- 02-Data-Types/data-types.js
+++ 02-Data-Types/data-types.js
@@ -1,1 +1,42 @@
+// String
+const name = "Chaitanya";
+console.log(typeof name);
 
+// Number
+const age = 28;
+console.log(typeof age);
+
+// Boolean
+const isEmployee = true;
+console.log(typeof isEmployee);
+
+// Undefined
+let city;
+console.log(typeof city);
+
+// Null
+let manager = null;
+console.log(manager);
+console.log(typeof manager);
+
+// BigInt
+const salary = 12345678901234567890123n;
+console.log(typeof salary);
+
+// Symbol
+const id = Symbol("id");
+console.log(typeof id);
+
+// Object
+const employee = {
+    name: "Chaitanya",
+    company: "APSS Media"
+};
+
+console.log(typeof employee);
+
+// Array
+const skills = ["JavaScript", "Node.js", "React"];
+
+console.log(typeof skills);
+console.log(Array.isArray(skills));
