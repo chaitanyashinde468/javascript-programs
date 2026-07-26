7d65f1160c670721fbd866c8079139d9a74502d6
Chaitanya Shinde <168747560+chaitanyashinde468@users.noreply.github.com>
2026-07-26 22:50:04 +0530

    Create hoisting.js

---
+++ 04-Hoisting/hoisting.js
@@ -0,0 +1,27 @@
+// Example 1 - var
+
+console.log(name);
+var name = "Chaitanya";
+
+// commented out the examples that would stop execution with an error. 
+// Uncomment them one at a time when testing.
+
+// Example 2 - let
+// console.log(city);
+let city = "Pune";
+
+// Example 3 - const
+// console.log(company);
+const company = "APSS Media";
+
+// Example 4 - Function Declaration
+greet();
+function greet() {
+    console.log("Hello");
+}
+
+// Example 5 - Function Expression
+// hello();
+var hello = function () {
+    console.log("Hi");
+};
